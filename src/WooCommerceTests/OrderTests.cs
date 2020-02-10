777fcb66fd01d7f436ebd6044fe90cdc5d70101e
maxims-skuvault <maxim.strelsov@skuvault.com>
2020-02-10 16:15:58 -0500

    GUARD-331 Remove tests for versions prior to 3.5, since we no longer support them

--- src/WooCommerceTests/OrderTests.cs
+++ src/WooCommerceTests/OrderTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WooCommerceTests
 {
-	[ TestFixture( "WP4_1_WC_2_4_credentials.csv" ) ]
+	[ TestFixture( "WP4_7_WC_3_6_credentials.csv" ) ]
 	[ TestFixture( "WP5_2_WC_3_6_credentials.csv" ) ]
 	public class OrderTests : BaseTest
 	{
