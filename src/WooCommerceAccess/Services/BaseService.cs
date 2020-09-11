46072a1a122054c3223525f564ac07cd8bdd2570
Bulat Farrakhov <bulat.farrakhov@skuvault.com>
2020-09-11 11:58:21 +0300

    GUARD-800: authorization tokens now are passed through url parameters for modern WooCommerce store versions too;

--- src/WooCommerceAccess/Services/BaseService.cs
+++ src/WooCommerceAccess/Services/BaseService.cs
@@ -44,10 +44,10 @@ namespace WooCommerceAccess.Services
 			if ( apiVersion == WooCommerceApiVersion.Unknown )
 				throw new WooCommerceException( "Unsupported WordPress and WooCommerce version!" );
 			
-			var legacyApiWcObject = new LegacyV3WCObject( new RestAPI( this.Config.ShopUrl + "wc-api/v3", this.Config.ConsumerKey, this.Config.ConsumerSecret, false ) );
+			var legacyApiWcObject = new LegacyV3WCObject( new RestAPI( this.Config.ShopUrl + "wc-api/v3", this.Config.ConsumerKey, this.Config.ConsumerSecret, authorizedHeader: false ) );
 				
 			if ( apiVersion == WooCommerceApiVersion.V3 )
-				this.WCObject = new ApiV3WCObject( new RestAPI( this.Config.ShopUrl + "wp-json/wc/v3/", this.Config.ConsumerKey, this.Config.ConsumerSecret ), legacyApiWcObject );
+				this.WCObject = new ApiV3WCObject( new RestAPI( this.Config.ShopUrl + "wp-json/wc/v3/", this.Config.ConsumerKey, this.Config.ConsumerSecret, authorizedHeader: false ), legacyApiWcObject );
 			else
 				this.WCObject = legacyApiWcObject;
 		}
