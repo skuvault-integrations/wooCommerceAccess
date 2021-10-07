b22eb46b90440a2d471872bcc76a13bb8431d0dc
Cristian William da Silva Vieira <cristian.silva@skuvault.com>
2021-10-07 17:56:45 -0300

    GUARD-2171 Changed the type of OrderId to string

---
+++ src/WooCommerceTests/WooCommerceOrderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using WooCommerceAccess.Models;
+
+namespace WooCommerceTests
+{
+	[ TestFixture ]
+	public class WooCommerceOrderTests
+	{
+		[ TestCase( "TEST@-0103", ExpectedResult = true, TestName = "Create_OrderWithLettersSymbolsNumbers_ReturnsTrue" ) ]
+		[ TestCase( "TEST0103", ExpectedResult = true, TestName = "Create_OrderWithLetterNumbers_ReturnsTrue" ) ]
+		[ TestCase( "0102", ExpectedResult = false, TestName = "Create_OrderWithOnlyNumbers_ReturnsFalse" ) ]
+		public bool Create_OrderIdWithSymbolsAndLetters( string orderId )
+		{
+			var orderWithLetters = new WooCommerceOrder
+			{
+				Id = orderId,
+				Number = "0001",
+				CreateDateUtc = DateTime.UtcNow,
+				UpdateDateUtc = DateTime.UtcNow,
+				Status = "pending",
+				Currency = "USD",
+				Total = 5,
+				Note = string.Empty,
+				WasPaid = false,
+				TotalTax = 0,
+				TotalDiscount = 0
+			};
+
+			int thrownAway;
+
+			return !int.TryParse( orderWithLetters.Id, out thrownAway );
+		}
+	}
+}
